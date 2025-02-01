05a75f755e35638709b31c73ebe8b45cb9a304f1
Nitish Prajapati <43167800+nitishprajapati5@users.noreply.github.com>
2025-02-01 22:04:05 +0530

    Changes in Details Page

--- src/app/details/[id]/page.js
+++ src/app/details/[id]/page.js
@@ -8,7 +8,7 @@ import { toast } from "sonner";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Activity, Copy, Edit, Eye, Leaf, Share, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle,DialogDescription } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useProgress } from "@/app/_contexts/ProgressContext";
